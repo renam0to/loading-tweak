47400800f76d383a562b957ff66c0b2396d17dea
Warren Tomo <warrentomo0719@gmail.com>
2024-12-21 14:31:40 +0800

    load.py

--- load.py
+++ load.py
@@ -2,9 +2,9 @@ import time
 
 def loading_screen():
     print("Loading, please wait...")
-    for i in range(1, 51):  # Loop from 1 to 100
-        time.sleep(0.05)  # Simulate loading delay
-        print(f"\rProgress: {i}%", end="")  # Update progress on the same line
+    for i in range(1, 51):  
+        time.sleep(0.05)  
+        print(f"\rProgress: {i}%", end="")  
     inpat = input("\nCLICK 'ENTER' TO CONTINUE")
     for z in range(51,101):
         time.sleep(0.05)
